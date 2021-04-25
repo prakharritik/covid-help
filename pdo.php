0767c62f932b3c36ab2038451163a265a21021b8
prakharritik <60786008+prakharritik@users.noreply.github.com>
2021-04-25 16:05:57 +0530

    added database access

--- pdo.php
+++ pdo.php
@@ -1,18 +1,16 @@
 <?php
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-               array(
-                'pdo.server' => array(
-                   'driver'   => 'pgsql',
-                   'user' => $dbopts["user"],
-                   'password' => $dbopts["pass"],
-                   'host' => $dbopts["host"],
-                   'port' => $dbopts["port"],
-                   'dbname' => ltrim($dbopts["path"],'/')
-                   )
-               )
-);
+    $HOST = $dbopts["host"];
+    $PORT = $dbopts["port"];
+    $DB_NAME = ltrim($dbopts["path"],'/');
+    $DB_USER = $dbopts["user"];
+    $DB_PASSWORD = $dbopts["pass"];
+    $pdo = new PDO(
+        //"mysql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
+        "pgsql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	/*$HOST = 'localhost';
     $PORT = '5432';
     $DB_NAME = 'covid';
