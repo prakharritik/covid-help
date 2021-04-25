1328431cbd3521f0a840f1e329284485cc77337b
prakharritik <60786008+prakharritik@users.noreply.github.com>
2021-04-25 17:04:50 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,17 +1,7 @@
 <?php
 
-$dbopts = parse_url(getenv('DATABASE_URL'));
-    $HOST = $dbopts["host"];
-    $PORT = $dbopts["port"];
-    $DB_NAME = ltrim($dbopts["path"],'/');
-    $DB_USER = $dbopts["user"];
-    $DB_PASSWORD = $dbopts["pass"];
-    $pdo = new PDO(
-        //"mysql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
-        "pgsql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	/*$HOST = 'localhost';
+
+	$HOST = 'localhost';
     $PORT = '5432';
     $DB_NAME = 'covid';
     $DB_USER = 'postgres';
@@ -20,4 +10,4 @@ $dbopts = parse_url(getenv('DATABASE_URL'));
         //"mysql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
         "pgsql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
     );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);*/
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
