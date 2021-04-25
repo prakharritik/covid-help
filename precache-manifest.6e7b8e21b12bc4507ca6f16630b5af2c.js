31e08d5ab0565e335e56edb0880b0342fa25f05d
prashant94157 <0166621m@gmail.com>
2021-04-25 15:59:58 +0530

    done some required changes in news.html

---
+++ precache-manifest.6e7b8e21b12bc4507ca6f16630b5af2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c12f43a053d0afcd6535e661f5dd11",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "39c91909c65ec52b578d",
+    "url": "/corona-tracker/static/css/main.f3e2a3e0.chunk.css"
+  },
+  {
+    "revision": "d31022c853da34026487",
+    "url": "/corona-tracker/static/js/2.cb98e94a.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/corona-tracker/static/js/2.cb98e94a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c91909c65ec52b578d",
+    "url": "/corona-tracker/static/js/main.96ccbe23.chunk.js"
+  },
+  {
+    "revision": "bf17874e7839ba08222a",
+    "url": "/corona-tracker/static/js/runtime-main.87fc3e90.js"
+  },
+  {
+    "revision": "1bdacb4d02b85c862a176655468a5f33",
+    "url": "/corona-tracker/static/media/OIP.1bdacb4d.jpg"
+  }
+]);
